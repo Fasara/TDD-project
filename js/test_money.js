83cbf925d52142183196c127c464157474912c53
Fabiana <fabiana.asara@gmail.com>
2024-04-29 17:35:58 +0100

    Adds first green test

--- js/test_money.js
+++ js/test_money.js
@@ -1,5 +1,14 @@
 const assert = require('assert');
 
+class Dollar {
+    constructor(amount) {
+        this.amount = amount;
+    }
+    times(multiplier) {
+        return new Dollar(this.amount * multiplier);
+    }
+}
+
 let fiver = new Dollar(5); // We create an object representing a 5 dollar bill
 let tenner = fiver.times(2); // We multiply it by 2
 assert.strictEqual(tenner.amount, 10); // we expect the result to be 10
