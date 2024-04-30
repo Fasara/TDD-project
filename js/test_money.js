66669feda2e6c2e08664f389a7b0432472d15bb0
Fabiana <fabiana.asara@gmail.com>
2024-04-30 21:23:47 +0100

    Adds money class and refactors test

--- js/test_money.js
+++ js/test_money.js
@@ -1,15 +1,16 @@
 const assert = require('assert');
-
-class Dollar {
-    constructor(amount) {
+class Money {
+    constructor(amount, currency) {
         this.amount = amount;
+        this.currency = currency;
     }
     times(multiplier) {
-        return new Dollar(this.amount * multiplier);
+        return new Money(this.amount * multiplier, this.currency);
     }
 }
 
-let fiver = new Dollar(5); // We create an object representing a 5 dollar bill
+let fiver = new Money(5, 'USD'); // We create a new Money object with 5 USD
 let tenner = fiver.times(2); // We multiply it by 2
 assert.strictEqual(tenner.amount, 10); // we expect the result to be 10
+assert.strictEqual(tenner.currency, 'USD'); // we expect the currency to be USD
 
