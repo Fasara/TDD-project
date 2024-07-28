0df8db461697c05f4cd63e75243c280a8ac0c04d
Fabiana <fabiana.asara@gmail.com>
2024-07-28 16:16:59 +0100

    feat: Add divide function and refactor tests

--- js/test_money.js
+++ js/test_money.js
@@ -7,10 +7,19 @@ class Money {
     times(multiplier) {
         return new Money(this.amount * multiplier, this.currency);
     }
+
+    divide(divider) {
+        return new Money(this.amount / divider, this.currency);
+    }
 }
 
 let fiver = new Money(5, 'USD'); // We create a new Money object with 5 USD
-let tenner = fiver.times(2); // We multiply it by 2
-assert.strictEqual(tenner.amount, 10); // we expect the result to be 10
-assert.strictEqual(tenner.currency, 'USD'); // we expect the currency to be USD
+let tenner = new Money(10, 'USD'); // We create a new Money object with 10 USD
+assert.deepStrictEqual(fiver.times(2), tenner);
+
+
+let originalMoney = new Money(4002, 'KRW');
+let actualMoneyAfterDivision = originalMoney.divide(4);
+let expectedMoneyAfterDivision = new Money(1000.5, 'KRW');
+assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision); // node.js assert module, it compares two objects for equality using '===' operator
 
